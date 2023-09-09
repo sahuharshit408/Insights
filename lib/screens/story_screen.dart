ba59931e8ac32b540a94e354bc35e6f603d6a03b
Ayush <ayushsrivastava1103@gmail.com>
2023-09-09 13:16:22 +0530

    firebase futher changes..

--- lib/screens/story_screen.dart
+++ lib/screens/story_screen.dart
@@ -1,11 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:ui';
-
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
 import 'package:insights/screens/full_screen_image.dart';
-
 import '../models/press_releases_model.dart';
 
 class StoryScreen extends StatefulWidget {
